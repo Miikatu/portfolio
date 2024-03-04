e77597561542cf48f7910f8ae1fc9952157e7f3b
miikatu <miikatti@gmail.com>
2024-03-05 00:01:31 +0200

    changed navButton hrefs

--- src/app/components/navigation/navButton.js
+++ src/app/components/navigation/navButton.js
@@ -3,7 +3,7 @@ export default function navButton(props) {
         <a className="hover:bg-slate-200
          bg-rounded
 		 text-black 
-         bg-clip-bprder border-8 border-transparent rounded-2xl" href={"portfolio/#"+props.target} >{props.field}</a>
+         bg-clip-bprder border-8 border-transparent rounded-2xl" href={"/#"+props.target} >{props.field}</a>
     )
 
 }
